9df817ac0bbbef768dfecb7075be2b69fc7b2bce
tao <tao@pop-os.localdomain>
2022-02-23 20:07:23 -0500

    Downloaded all pdfs, created dict of test names - santiago

--- config.py
+++ config.py
@@ -6,3 +6,33 @@ PAGE_DIM = {'default_reading':[(10, 30, 290, 750),(310, 30, 600, 750)],
                 'first_writing':[(10, 410, 290, 750),(310, 410, 600, 750)],
                 'end_writing':[(10, 30, 290, 650),(310, 30, 600, 650)]
                 }
+
+TESTS_NAMES = {
+    'test1':{'College Board Practice Test 1'},
+    'test2':{'College Board Practice Test 2'},
+    'test3':{'College Board Practice Test 3'},
+    'test4':{'College Board Practice Test 4'},
+    'test5':{'May 2016 US QAS', 'College Board Practice Test 5'},
+    'test6':{'May 2016 International QAS', 'College Board Practice Test 6'},
+    'test7':{'October 2016 QAS', 'College Board Practice Test 7'},
+    'test8':{'January 2017 QAS', 'College Board Practice Test 8'},
+    'test9':{'April 2017 School Day QAS', 'May 2017 International QAS'},
+    'test10':{'May 2017 US QAS'},
+    'test11':{'October 2017 QAS', 'College Board Practice Test 9'},
+    'test12':{'March 2018 QAS'},
+    'test13':{'April 2018 School Day QAS', 'May 2018 International QAS'},
+    'test14':{'May 2018 US QAS'},
+    'test15':{'October 2018 QAS', 'College Board Practice Test 10'},
+    'test16':{'March 2019 QAS'},
+    'test17':{'April 2019 School Day QAS'},
+    'test18':{'May 2019 US QAS'},
+    'test19':{'May 2019 International QAS'},
+    'test20':{'October 2019 QAS'},
+    'test21':{'March 2020 QAS'},
+    'test22':{'October 2020 QAS'},
+    'test23':{'March 2021 QAS'},
+    'test24':{'April 2021 School Day QAS'},
+    'test25':{'May 2021 US QAS'},
+    'test26':{'May 2021 International QAS'},
+    'test27':{'October 2021 QAS'}
+}
